083b3a9e229daacacb5d109b74e94a95a4ba7801
Blake Justice <13934264+bljustice@users.noreply.github.com>
2023-09-19 11:11:40 -0400

    Add actual knob and tick

--- src/gui/knob.rs
+++ src/gui/knob.rs
@@ -1,4 +1,4 @@
-use nih_plug_vizia::vizia::{prelude::*, view::View, views};
+use nih_plug_vizia::vizia::{prelude::*, view::View, views::{Knob, TickKnob, ArcTrack}};
 
 pub struct KnobContainer {}
 
@@ -24,7 +24,35 @@ impl KnobContainer {
         Self {}.build(cx, |cx| {
             VStack::new(cx, |cx| {
                 Label::new(cx, &label);
-                views::Knob::new(cx, 0.5, lens, false).on_changing(on_change_callback);
+                Knob::custom(
+                    cx,
+                    0.5,
+                    lens,
+                    move |cx, lens| {
+                        TickKnob::new(
+                            cx,
+                            Percentage(80.),
+                            Pixels(2.),
+                            Percentage(75.),
+                            270.,
+                            KnobMode::Continuous,
+                        )
+                        .value(lens.clone())
+                        .class("tick");
+                        ArcTrack::new(
+                            cx,
+                            false,
+                            Percentage(100.),
+                            Percentage(10.),
+                            -135.,
+                            135.,
+                            KnobMode::Continuous,
+                        )
+                        .value(lens)
+                        .class("track")
+                    }
+                )
+                .on_changing(on_change_callback);
                 Label::new(cx, value_lens);
             })
             .child_space(Stretch(1.0));
